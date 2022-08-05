a855a8a9fcb184ce64a90799b56f26deba232df8
simone-art <simoneperiodista3@gmail.com>
2022-08-05 17:59:13 -0300

    Alterando o código pra obter o response e criar o User com sucesso

--- src/main/java/io/github/simone/art/quarkussocial/rest/UserResource.java
+++ src/main/java/io/github/simone/art/quarkussocial/rest/UserResource.java
@@ -2,15 +2,22 @@ package io.github.simone.art.quarkussocial.rest;
 
 import io.github.simone.art.quarkussocial.rest.dto.CreateUserRequest;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/users")
+// Anotação consumes permite definir o tipo de body a ser recebido
+@Consumes(MediaType.APPLICATION_JSON)
+// Anotação produces indica o tipo de body a obter na resposta
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
     @POST
     public Response createUser(CreateUserRequest createUserRequest){
-        return Response.ok().build();
+        return Response.ok(createUserRequest).build();
 
     }
 
